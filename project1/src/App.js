136db5e8429280ae00658eb2d97f12c32765f605
devkgn88 <devkgn88@gmail.com>
2025-04-27 00:32:48 +0900

    [김가남] 클린업을 이용하여 언마운트 제어하기

--- project1/src/App.js
+++ project1/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useRef, useEffect, useState } from 'react';
 import Viewer from './component/Viewer';
 import Controller from './component/Controller';
+import Even from './component/Even';
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -12,27 +13,32 @@ const App = () => {
   const handleChangeText = (e) =>{
     setText(e.target.value);
   };
-  const didMountRef = useRef(false);
-  useEffect(() => {
-    if(!didMountRef.current){
-      didMountRef.current = true;
-      return;
-    } else{
-      console.log('컴포넌트 업데이트');
-    }
-  });
-  useEffect(() =>{
-    console.log('컴포넌트 마운트');
-  },[]);
-  useEffect(()=>{
-    const intervalID = setInterval(() =>{
-      console.log("깜빡");
-    },1000);
-    return () => {
-      console.log('클린업');
-      clearInterval(intervalID);
-    }
-  });
+  // 첫번째 랜더링 시점 제외
+  // const didMountRef = useRef(false);
+  // useEffect(() => {
+  //   if(!didMountRef.current){
+  //     didMountRef.current = true;
+  //     return;
+  //   } else{
+  //     console.log('컴포넌트 업데이트');
+  //   }
+  // });
+
+  // 첫번째 마운트 될때 동작
+  // useEffect(() =>{
+  //   console.log('컴포넌트 마운트');
+  // },[]);
+
+  // 클린업 사용하기
+  // useEffect(()=>{
+  //   const intervalID = setInterval(() =>{
+  //     console.log("깜빡");
+  //   },1000);
+  //   return () => {
+  //     console.log('클린업');
+  //     clearInterval(intervalID);
+  //   }
+  // });
   return(
     <div className='App'>
       <h1>Simple Counter</h1>
@@ -42,6 +48,7 @@ const App = () => {
       </section>
       <section>
         <Viewer count={count}/>
+        {count % 2 === 0 && <Even/>}
       </section>
       <section>
         <Controller handleSetCount={handleSetCount}/>
